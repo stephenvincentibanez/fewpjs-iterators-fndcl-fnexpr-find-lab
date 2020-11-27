af6aafd8a07d4ddba058be9ba6c56027f87c96e7
Steve <stephenvincentibanez@gmail.com>
2020-11-27 13:03:43 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// function superbowlWin(record){
+//   record.find(function(r))
+// }
+
+function superbowlWin(record) {
+  const winner =  record.find(r => {
+    return r.result === "W";
+  });
+  if (winner) {
+    return winner.year;
+  } else {
+    return undefined
+  }
+};
